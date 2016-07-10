ef1c653a25d2beb90aed2e3b6cca40c815c44e01
Tom Z <tcz104@gmail.com>
2016-07-09 22:43:44 -0400

    add put todos

--- server.js
+++ server.js
@@ -113,6 +113,39 @@ app.delete('/todos/:id', function(req, res){
 
 });
 
+//PUT /todos/:id
+
+app.put('/todos/:id', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	var todoId = parseInt(req.params.id, 10);
+	var found = _.findWhere(todos, {id: todoId});
+
+	if (!found){
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}else {
+		//never provided value, no problem
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) || body.description.trim().length === 0){
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}else {
+		//never provided value, no problem
+	}	
+
+	_.extend(found, validAttributes);
+
+	res.json(found);
+});
+
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT)
 });
