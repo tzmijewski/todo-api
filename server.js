580522d30946080da9069af367f7bffebc63298e
Tom Z <tcz104@gmail.com>
2016-07-09 23:08:08 -0400

    added get completed filter

--- server.js
+++ server.js
@@ -17,10 +17,19 @@ app.get('/', function(req, res){
 	res.send('Todo API Root');
 });
 
-//GET /todos
+//GET /todos?completed=false
 
 app.get('/todos', function(req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 //GET /todos/:id
