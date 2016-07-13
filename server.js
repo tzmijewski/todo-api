4249513e03073bea5bf100c546db19bd542ceb68
Tom Z <tcz104@gmail.com>
2016-07-12 22:46:03 -0400

    :q1

--- server.js
+++ server.js
@@ -268,6 +268,21 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create({
+		email: body.email,
+		password: body.password
+	}).then(function(user) {
+		res.json(user);
+	}).catch(function(e) {
+		if (e) {
+			res.status(400).json(e)
+		}
+	});
+});
+
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT)
